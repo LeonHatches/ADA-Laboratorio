c2cd0d4a592b71f039c0891b4b4533012409957f
leonhatches <jhatches@unsa.edu.pe>
2025-09-14 17:09:40 -0500

    Lab 01 - Decisión: se crea el algoritmo para verificar palíndromo

--- Laboratorios/Lab 01/decision.cpp
+++ Laboratorios/Lab 01/decision.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 bool palindromo (string str) {
-    
+    int size = str.length() / 2;
+
+    for (int i = 0 ; i < size ; i++) {
+        if (str[i] != str[str.length() - i])
+            return false;
+    }
+
+    return true;
 }
 
 int main () {
