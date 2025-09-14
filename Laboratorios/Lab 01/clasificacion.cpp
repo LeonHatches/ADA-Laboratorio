5b3e724c8886b22afbd40270c186e728bb0e6bd4
leonhatches <jhatches@unsa.edu.pe>
2025-09-14 16:34:42 -0500

    Lab 01 - clasificación: se crea el método para clasificar de menor a mayor

---
+++ Laboratorios/Lab 01/clasificacion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void ordenamiento (vector <string> lista) {
+    
+    for (int j, i = 1 ; i < lista.size() ; i++) {
+        
+        string clave = lista[i];
+
+        for (j = i-1 ; j >= 0 && lista[j] > clave ; i--)
+            lista[j+1] = lista[j];
+        
+        lista[j+1] = clave;
+    }
+}
+
+int main () {
+    
+    return 0;
+}
