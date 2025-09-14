6d32488c0e28bc1990b91d89ef07ff425d1316e1
leonhatches <jhatches@unsa.edu.pe>
2025-09-13 22:16:27 -0500

    Lab 01 - MCD: estructura de las funciones para hallar el MCD

---
+++ Laboratorios/Lab 01/mcd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int algoritmoBucle (int numero1, int numero2) {
+    
+}
+
+int algoritmoEuclides (int numero1, int numero2) {
+
+}
+
+int main () {
+    
+}
