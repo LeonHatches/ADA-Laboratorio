84485a6f69fc2a03b840ddfca84893d2443d2e0f
leonhatches <jhatches@unsa.edu.pe>
2025-09-14 12:01:01 -0500

    Lab 01 - Búsqueda: se corrige el método de búsqueda

--- Laboratorios/Lab 01/busqueda.cpp
+++ Laboratorios/Lab 01/busqueda.cpp
@@ -5,8 +5,9 @@ int algoritmoBusqueda (string clave, string texto) {
     if (texto.length() - clave.length() < 0)
         return -1;
 
-    for (int i = 0 ; i < texto.length() - clave.length() ; i++) {
-        if (texto.substr(i, i + clave.length()) == clave)
+    for (int i = 0 ; i <= texto.length() - clave.length() ; i++) {
+        
+        if (texto.substr(i, clave.length()) == clave)
             return i;
     }
     
@@ -14,5 +15,16 @@ int algoritmoBusqueda (string clave, string texto) {
 }
 
 int main () {
+    string palabra = "final";
+    string texto   =
+    "Un algoritmo puede dividir un problema grande en partes mas pequenas resolviendo cada parte por separado y luego combinando los resultados este metodo permite reducir la complejidad y encontrar soluciones de manera mas eficiente aplicando la idea de dividir vencer y construir la respuesta paso a paso hasta llegar al resultado final";
+    
+    int pos = algoritmoBusqueda(palabra, texto);
+    
+    if (pos == -1)
+        cout<<"No se encontró la palabra."<<endl;
+    else
+        cout<<"La palabra se encuentra en: "<<pos<<endl;   
+    
     return 0;
 }
