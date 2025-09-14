6eeee8a6bafbe93b750d0425590a92ae8bf04693
leonhatches <jhatches@unsa.edu.pe>
2025-09-14 11:42:21 -0500

    Lab 01 - Búsqueda: se crea el método para hallar la clave

--- Laboratorios/Lab 01/busqueda.cpp
+++ Laboratorios/Lab 01/busqueda.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int algoritmoBusqueda (string clave, string texto) {
+    if (texto.length() - clave.length() < 0)
+        return -1;
 
+    for (int i = 0 ; i < texto.length() - clave.length() ; i++) {
+        if (texto.substr(i, i + clave.length()) == clave)
+            return i;
+    }
+    
+    return -1;
 }
 
 int main () {
