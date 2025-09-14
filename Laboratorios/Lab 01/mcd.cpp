d557aecc38d8ab3b9705a7bc3a3c8b15a20727e7
leonhatches <jhatches@unsa.edu.pe>
2025-09-13 22:26:20 -0500

    Lab 01 - MCD: algoritmo de Euclides completado

--- Laboratorios/Lab 01/mcd.cpp
+++ Laboratorios/Lab 01/mcd.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 
 int algoritmoBucle (int numero1, int numero2) {
-    
+    return 0;
 }
 
 int algoritmoEuclides (int numero1, int numero2) {
+    int residuo;
 
+    do {
+        residuo = numero1 % numero2;
+        numero1 = numero2;
+        numero2 = numero1;
+
+    } while (numero2 != 0);
+    
+    return numero1;
 }
 
 int main () {
-    
+    const int NUMERO1 = 12, NUMERO2 = 21;
+    int resultadoBucles, resultadoEuclides;
+
+    resultadoBucles   = algoritmoBucle(NUMERO1, NUMERO2);
+    resultadoEuclides = algoritmoEuclides(NUMERO1, NUMERO2);
+
 }
