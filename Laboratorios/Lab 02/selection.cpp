f25fe0869e67d0ed3db48c13c5b99cfe8f7b0c3a
leonhatches <jhatches@unsa.edu.pe>
2025-09-15 10:32:18 -0500

    Lab 02 - Selection: se cambia el nombre del archivo y se implementa los bucles

---
+++ Laboratorios/Lab 02/selection.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selection (vector <int> &arr) {
+    
+
+    for (int min, i = 0 ; i < arr.size()-2 ; i++) {
+        
+        for (int j = i ; i < arr.size()-1 ; i--) {
+            // Compara
+        }
+    }
+}
+
+int main () {
+
+}
