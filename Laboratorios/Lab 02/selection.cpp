c38fe56a07dcdf507df2e2ca81b898d47eff3b69
leonhatches <jhatches@unsa.edu.pe>
2025-09-15 10:41:46 -0500

    Lab 02 - Selection: se completa el algoritmo primera versión

--- Laboratorios/Lab 02/selection.cpp
+++ Laboratorios/Lab 02/selection.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 using namespace std;
 
 void selection (vector <int> &arr) {
-    
 
     for (int min, i = 0 ; i < arr.size()-2 ; i++) {
         
-        for (int j = i ; i < arr.size()-1 ; i--) {
-            // Compara
+        for (int j = i ; i < arr.size()-1 ; i++) {
+            
+            if (arr[i] > arr[j])
+                min = j;
         }
+        swap(arr[i], arr[min]);
     }
 }
 
 int main () {
-
+    
 }
