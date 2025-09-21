eb9e849d732e55e56521a07d785436a3ec6f548a
leonhatches <jhatches@unsa.edu.pe>
2025-09-20 23:39:41 -0500

    Lab 02 - Ejercicio03: se corrige el error de usar el peor caso y se agregan los métodos

--- Laboratorios/Lab 02/Ejercicio03.cpp
+++ Laboratorios/Lab 02/Ejercicio03.cpp
@@ -17,6 +17,20 @@ void insertion (vector <T>& arr) {
     }
 }
 
+void selection (vector <int> &arr) {
+
+    for (int min, i = 0 ; i < arr.size()-1 ; i++) {
+        
+        min = i;
+        for (int j = min+1 ; j < arr.size() ; j++) {
+            
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+
 int main () {
     vector <int> cantidades = {1000, 5000, 10000};
     
@@ -24,26 +38,26 @@ int main () {
 
         // Se crea los vectores de 1k, 5k y 10k
         vector <int> list1 (cantidades[i], 0);
-        for (int n = 0, i = list1.size() ; i > 0 ; i--)
-            { list1[n] = i; n++; }
+        for (int i = 0; i < list1.size() ; i++)
+            list1[i] = i;
         
         vector <int> list2 = list1;
 
         // Toma de tiempo y método DE INSERCIÓN
         auto start1 = chrono::high_resolution_clock::now();
-
+        insertion(list1);
         auto end1 = chrono::high_resolution_clock::now();
 
         chrono::duration<double> duration1 = end1 - start1;
 
         // Toma de tiempo y método DE SELECCIÓN
         auto start2 = chrono::high_resolution_clock::now();
-
+        selection(list2);
         auto end2 = chrono::high_resolution_clock::now();
 
         chrono::duration<double> duration2 = end2 - start2;
 
         // Mostrar mensaje
-        cout<<"| Iteraciones de "<<cantidades[i]<<"| Ins: "<<duration1.count()<<" Sel: "<<duration2.count()<<endl;
+        cout<<"| Iteraciones de "<<cantidades[i]<<" | Ins: "<<duration1.count()<<" Sel: "<<duration2.count()<<endl;
     }
 }
