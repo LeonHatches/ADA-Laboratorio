9bc6e2c2b6e6f4401236b409c30131541fa41280
leonhatches <jhatches@unsa.edu.pe>
2025-09-20 22:11:23 -0500

    Lab 02 - Ejercicio01: se agregan los vectores a probar, main y correciones

--- Laboratorios/Lab 02/Ejercicio01.cpp
+++ Laboratorios/Lab 02/Ejercicio01.cpp
@@ -5,8 +5,8 @@ using namespace std;
 template <typename T>
 void insertion (vector <T>& arr) {
    
-    int pasos = 1;
-    cout<<"Pasos: "<<ends;
+    int pasos = 0;
+    cout<<"Pasos: "<<pasos<<" "<<ends;
 
     for (int j, i = 1 ; i < arr.size() ; i++) {
         
@@ -14,14 +14,27 @@ void insertion (vector <T>& arr) {
 
         for (j = i-1 ; j >= 0 && arr[j] > clave ; j--) {
             arr[j+1] = arr[j];
-            cout<<pasos<<" "<<ends;
             pasos++;
+            cout<<pasos<<" "<<ends;
         }
         
         arr[j+1] = clave;
     }
+
+    cout<<endl;
 }
 
 int main () {
+    vector <string> orden = {"Alberto", "Banu", "Carlos", "Dante", "Esmeralda", "Fernando"};
+    vector <string> reves = {"Fernando", "Esmeralda", "Dante", "Carlos", "Banu", "Alberto"};
+    vector <string> aleat = {"Carlos", "Banu", "Alberto", "Esmeralda", "Fernando", "Dante"};
+
+    insertion(orden);
+    cout<<"Lista ordenada: "<<ends;
+    
+    for (string nombre : orden)
+        cout<<nombre<<" "<<ends;
+    
+    cout<<endl;
     
 }
