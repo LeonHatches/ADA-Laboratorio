0821497f284767ec926c045f7d659eeec2603ce8
leonhatches <jhatches@unsa.edu.pe>
2025-09-22 10:55:56 -0500

    Lab 03 - Quicksort: se crea el método de partición

---
+++ Laboratorios/Lab 03/Quicksort.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low ; j <= high - 1 ; j++) {
+
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
