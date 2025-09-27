a2e4c897e85f47928f4d6d4ad69cf49b44cbf228
leonhatches <jhatches@unsa.edu.pe>
2025-09-27 18:49:47 -0500

    Lab 02 - Ejercicio 2: se agrega el Quicksort con pivote random

--- Laboratorios/Lab 03/Ejercicio2.cpp
+++ Laboratorios/Lab 03/Ejercicio2.cpp
@@ -25,3 +25,41 @@ void quickSortFixed (vector <T>& arr, int low, int high) {
         quickSortFixed (arr, pi + 1, high);
     }
 }
+
+template <typename T>
+int partitionRandom (vector <T>& arr, int low, int high) {
+    
+    int random = low + rand() % (high - low - 1);
+    swap(arr[random], arr[high]);
+    T pivot = arr[high];
+    
+    int i = low - 1;
+
+    for (int j = low ; j <= high - 1 ; j++) {
+        
+        if (arr[j] < pivot)
+            swap(arr[++i], arr[j]);
+    }
+
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+template <typename T>
+void quickSortRandom (vector <T>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partitionRandom (arr, low, high);
+        quickSortRandom (arr, low, pi - 1);
+        quickSortRandom (arr, pi + 1, high);
+    }
+}
+
+
+
+
+
+
+
+
+
+
