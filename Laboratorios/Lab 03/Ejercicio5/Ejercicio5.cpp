cc3b24ea014826526321187a24f07a9047265046
leonhatches <jhatches@unsa.edu.pe>
2025-09-27 20:15:03 -0500

    Lab 05 - Ejercicio 5: se agregan los métodos genéricos del merge y quick sort

--- Laboratorios/Lab 03/Ejercicio5/Ejercicio5.cpp
+++ Laboratorios/Lab 03/Ejercicio5/Ejercicio5.cpp
@@ -31,3 +31,84 @@ class Producto {
             return "| "+to_string(id)+", "+nombre+", "+to_string(precio)+" |";
         }
 };
+
+//---------------------- MERGESORT ---------------------- 
+template <typename T>
+void merge(vector<T> &arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    vector<T> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    
+    int i = 0, j =0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] < R[j])
+            arr[k++] = L[i++];
+        else 
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+template <typename T>
+void mergeSort(vector<T> &arr, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+template <typename T>
+void mergeSort(vector<T> &arr) {
+    mergeSort(arr, 0, arr.size() - 1);
+}
+
+// -------------------- QUICKSORT -----------------------
+
+template <typename T>
+int partition(vector <T>& arr, int low, int high) {
+    T pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low ; j <= high - 1 ; j++) {
+        
+        if (arr[j] < pivot)
+            swap(arr[++i], arr[j]);
+    }
+
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+template <typename T>
+void quickSort (vector <T>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+template <typename T>
+void quickSort (vector <T>& arr) {
+    quickSort (arr, 0, arr.size() - 1);
+}
+
+// -----------------------------------------------------------
+
+
+
+
+
