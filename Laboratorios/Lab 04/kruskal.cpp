acabc545baff60c657feb6f7c38bd781a97e76a6
leonhatches <jhatches@unsa.edu.pe>
2025-10-11 17:35:01 -0500

    Lab 04 - Kruskal: se implementa el recorrido de la aristas, agregar elementos y merge

--- Laboratorios/Lab 04/kruskal.cpp
+++ Laboratorios/Lab 04/kruskal.cpp
@@ -7,13 +7,13 @@ using namespace std;
 template <typename T>
 GraphLink<T> kruskal (GraphLink<T>& G) {
     
-    vector<Vertex<T>*> set;
+    vector<Vertex<T>*> vertices;
     for (auto v : G.getListVertex()) {
-        set.push_back(v);
+        vertices.push_back(v);
     }
 
     vector<tuple<Vertex<T>*, Vertex<T>*, int>> Q;
-    for (auto v : set) {
+    for (auto v : vertices) {
         for (const auto& e : v->getAdj()) {
             Q.push_back({v, e.getDest(), e.getWeight()});
         }
@@ -24,10 +24,41 @@ GraphLink<T> kruskal (GraphLink<T>& G) {
         return get<2>(a) < get<2>(b);
     });
 
-    int n = set.size();
+    int n = vertices.size();
+    vector<int> comp (n);
+    for (size_t i = 0; i < n; ++i) {
+        comp[i] = i;
+    }
+    
+    auto findIndex = [&](Vertex<T>* v) {
+        for (int i = 0, i < n ; ++i)
+            if (vertices[i] == v)
+                return i;
+        
+        return -1;
+    };
+
     GraphLink<T> tree;
 
-    return G;
+    for (const auto& [u, v, w] : Q) {
+        int iU = findIndex(u);
+        int iV = findIndex(v);
+
+        if(comp[iU] != comp[iV]) {
+            tree.insertVertex(u->getData());
+            tree.insertVertex(v->getData());
+            tree.insertEdge(u->getData(), v->getData(), w);
+
+            int oldComp = comp[iV];
+            int newComp = como[iU];
+
+            for(auto& c : comp)
+                if(c == oldComp)
+                    c = newComp;
+        }
+    }
+    
+    return tree;
 }
 
 int main () {
