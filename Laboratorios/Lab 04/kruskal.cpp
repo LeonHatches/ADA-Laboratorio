407197209861a768f8f4d3b376301dcab051a78d
leonhatches <jhatches@unsa.edu.pe>
2025-10-11 16:47:57 -0500

    Lab 04 - Kruskal: se agrega el conjunto de vértices y aristas

--- Laboratorios/Lab 04/kruskal.cpp
+++ Laboratorios/Lab 04/kruskal.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include "graph.h"
+#include <vector>
 
 using namespace std;
 
 template <typename T>
 GraphLink<T> kruskal (GraphLink<T>& G) {
     
+    vector<Vertex<T>*> set;
+    for (auto v : G.listVertex) {
+        set.push_back(v);
+    }
+
+    vector<Edge<T>*> Q;
+    for (auto v : set) {
+        for (auto& e : v->getAdj()) {
+            Q.push_back(new Edge (v, e.getWeight(), e.getDest()));
+        }
+    }
+
+    GraphLink<T> tree;
+
     return G;
 }
 
 int main () {
-    
+
 }
