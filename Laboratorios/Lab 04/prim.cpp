c78654287d7a541bf2c87f3807f5eeda29096c9b
leonhatches <jhatches@unsa.edu.pe>
2025-10-11 18:18:11 -0500

    Lab 04 - Prim: se crea la estructura de prim

---
+++ Laboratorios/Lab 04/prim.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "graph.h"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+GraphLink<T> prim (GraphLink<T>& G, T start) {
+    
+}
+
+int main () {
+
+}
