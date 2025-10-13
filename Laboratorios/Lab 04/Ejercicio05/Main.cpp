04c27986768f692a55ab3075b9ab56f1544eec9e
alecamic <achoquesa@unsa.edu.pe>
2025-10-12 22:13:01 -0500

    creación del grafo

--- Laboratorios/Lab 04/Ejercicio05/Main.cpp
+++ Laboratorios/Lab 04/Ejercicio05/Main.cpp
@@ -29,3 +29,50 @@ public:
         adj.push_back(Edge<T>(this, dest, weight));
     }
 };
+
+template <typename T>
+class Edge {
+private:
+    Vertex<T>* origin;
+    Vertex<T>* dest;
+    int weight;
+public:
+    Edge(Vertex<T>* o, Vertex<T>* d, int w) : origin(o), dest(d), weight(w) {}
+    
+    Vertex<T>* getOrigin() const { return origin; }
+    Vertex<T>* getDest() const { return dest; }
+    int getWeight() const { return weight; }
+};
+
+template <typename T>
+class GraphLink {
+private:
+    vector<Vertex<T>*> vertices;
+public:
+    ~GraphLink() {
+        for (auto v : vertices) delete v;
+    }
+    
+    void insertVertex(T data) {
+        vertices.push_back(new Vertex<T>(data));
+    }
+    
+    void insertEdge(T from, T to, int weight) {
+        Vertex<T>* v1 = findVertex(from);
+        Vertex<T>* v2 = findVertex(to);
+        if (v1 && v2) {
+            v1->addEdge(v2, weight);
+            v2->addEdge(v1, weight); // Para grafo no dirigido
+        }
+    }
+    
+    Vertex<T>* findVertex(T data) {
+        for (auto v : vertices) {
+            if (v->getData() == data) return v;
+        }
+        return nullptr;
+    }
+    
+    vector<Vertex<T>*> getListVertex() const {
+        return vertices;
+    }
