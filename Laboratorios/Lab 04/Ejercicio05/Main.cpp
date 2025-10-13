e4c108826ce670a65c7f589961340b5effd7067a
alecamic <achoquesa@unsa.edu.pe>
2025-10-12 22:02:15 -0500

    estrcutura base

--- Laboratorios/Lab 04/Ejercicio05/Main.cpp
+++ Laboratorios/Lab 04/Ejercicio05/Main.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+#include <climits>
+using namespace std;
+using namespace std::chrono;
+
+template <typename T>
+class Edge;
+
 template <typename T>
 class Vertex {
 private:
@@ -5,21 +20,12 @@ private:
     list<Edge<T>> adj;
 public:
     Vertex(T d) : data(d) {}
+    
     T getData() const { return data; }
     list<Edge<T>>& getAdj() { return adj; }
+    const list<Edge<T>>& getAdj() const { return adj; }
+    
     void addEdge(Vertex<T>* dest, int weight) {
         adj.push_back(Edge<T>(this, dest, weight));
     }
-};
-
-template <typename T>
-class Edge {
-private:
-    Vertex<T>* origin;
-    Vertex<T>* dest;
-    int weight;
-public:
-    Edge(Vertex<T>* o, Vertex<T>* d, int w) : origin(o), dest(d), weight(w) {}
-    Vertex<T>* getDest() const { return dest; }
-    int getWeight() const { return weight; }
 };
