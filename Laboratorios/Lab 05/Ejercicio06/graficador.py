5f968b142f2c9fca49035ecb010bf2089d72a423
leonhatches <jhatches@unsa.edu.pe>
2025-10-26 11:05:13 -0500

    Lab 05 - graficador: se modifica como se visualiza el grafo para usar las posiciones del mapa

--- Laboratorios/Lab 05/Ejercicio06/graficador.py
+++ Laboratorios/Lab 05/Ejercicio06/graficador.py
@@ -23,8 +23,6 @@ def graficar_grafo(grafo : GraphLink, posiciones=None, mapa=True):
         posiciones = nx.spring_layout(G)
 
     plt.figure(figsize=(8, 12))
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True)
     plt.xlim(-82, -68)
     plt.ylim(-19, 0)
     plt.axis("equal")
@@ -32,17 +30,43 @@ def graficar_grafo(grafo : GraphLink, posiciones=None, mapa=True):
     # MAPA BASE
     if mapa:
         mapa_grafo = get_graph()
+        G_mapa = nx.Graph()
 
-        for vertices in mapa_grafo.vertices:
-            G.add_node(vertices.data)
+        for v in mapa_grafo.vertices:
+            for e in v.adj_list:
+                if not G_mapa.has_edge(v.data, e.dest.data):
+                    G_mapa.add_edge(v.data, e.dest.data)
 
-        for vertex in mapa_grafo.vertices:
-            for edge in vertex.adj_list:
-                if not G.has_edge(vertex.data, edge.dest.data):
-                    G.add_edge(vertex.data, edge.dest.data, weight=edge.weight)
-        
         mapa_pos = get_posiciones()
-        nx.draw_networkx_edges(G, mapa_pos, edge_color='black', width=1.5)
+        nx.draw_networkx_edges(G_mapa, mapa_pos, edge_color='black', width=1)
+
+    # VÉRTICES DEL GRAFO
+    nx.draw_networkx_nodes(
+        G,
+        posiciones,
+        node_color='red',
+        node_size=100
+    )
+
+    # ARISTAS DEL GRAFO
+    nx.draw_networkx_edges(
+        G,
+        posiciones,
+        edge_color='blue',
+        style='dashed',
+        width=2
+    )
+
+    # ETIQUETAS DEL GRAFO
+    etiquetas = {n: n for n in G.nodes()}
+    nx.draw_networkx_labels(
+        G,
+        posiciones,
+        labels=etiquetas,
+        font_size=8,
+        font_color='black',
+        verticalalignment='bottom'
+    )
 
     plt.axis('off')
     plt.show()
