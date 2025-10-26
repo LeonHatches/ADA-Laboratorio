be5feef8cd468838c09eb7b824db952158dd58f4
leonhatches <jhatches@unsa.edu.pe>
2025-10-25 19:57:04 -0500

    Lab 05 - Floyd Warshall: se corrige la interpretación de la matriz de adyacencia

--- Laboratorios/Lab 05/utils/floyd_warshall.py
+++ Laboratorios/Lab 05/utils/floyd_warshall.py
@@ -18,10 +18,10 @@ def floyd_warshall (g: GraphLink):
             dist[i][j] = edge.weight
 
     # FLOYD WARSHALL
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                if dist[j][i] + dist[i][k] < dist [j][k]:
-                    dist[j][k] = dist[j][i] + dist[i][k]
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist [i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
     
     return dist, index
