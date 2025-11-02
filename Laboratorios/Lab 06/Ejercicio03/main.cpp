eb264bb66ff12ef2521f3d1c4173f764b53633c1
leonhatches <jhatches@unsa.edu.pe>
2025-11-02 17:43:01 -0500

    Lab 06 - Ejercicio03: se agrega comentarios para explicar el funcionamiento del código.

--- Laboratorios/Lab 06/Ejercicio03/main.cpp
+++ Laboratorios/Lab 06/Ejercicio03/main.cpp
@@ -8,12 +8,17 @@ pair <int, vector<int>> backpack (vector<int>& peso, vector<int>& valor, int W) 
     
     vector<vector<int>> dp (peso.size() + 1, vector<int>(W + 1, 0));
 
+    // Recorremos objetos
     for (int i = 1 ; i <= peso.size() ; i++) {
+        
+        // Recorremos todas las capacidades posibles
         for (int j = 0 ; j <= W ; j++) {
-
+            
+            // Si el objeto cabe en la mochila
             if (peso[i-1] <= j)
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j - peso[i-1]] + valor[i-1]);
             
+            // Si no cabe, copio el valor anterior
             else
                 dp[i][j] = dp[i-1][j];
         }
@@ -21,7 +26,11 @@ pair <int, vector<int>> backpack (vector<int>& peso, vector<int>& valor, int W) 
 
     vector<int> objetos;
     int j = W;
+
+    // Se va hacia atrás en la matriz dp
     for (int i = peso.size(); i > 0 ; i--) {
+        
+        // Si el valor cambia, entonces el obj fue tomado
         if (dp[i][j] != dp[i-1][j]) {
             objetos.push_back(i-1);
             j -= peso[i-1];
