9977ee37ca0a3c41ea3f35b08ef590ef03dbad16
leonhatches <jhatches@unsa.edu.pe>
2025-11-16 18:31:29 -0500

    Lab 07 - Caso 01: Se crea un main para probar el mergesort

---
+++ Laboratorios/Lab 07/Caso01/main.py
@@ -0,0 +1,12 @@
+# from ordenar_pacientes import merge_sort_pacientes
+
+pacientes = [
+    {"nombre": "Juan", "gravedad": 8, "tiempo": 3},
+    {"nombre": "Jose", "gravedad": 4, "tiempo": 2},
+    {"nombre": "Carl", "gravedad": 10, "tiempo": 4},
+    {"nombre": "Aron", "gravedad": 2, "tiempo": 1},
+    {"nombre": "Mark", "gravedad": 5, "tiempo": 5}
+]
+
+# Utilizar divide y venceras para ordenar pacientes
+# ordenados = merge_sort_pacientes(pacientes)
