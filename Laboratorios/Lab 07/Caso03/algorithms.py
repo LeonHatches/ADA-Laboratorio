f55df90e21b77d8949896ac5355d9cfeee7cacb1
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-11-23 17:50:07 -0500

    Lab 07 - Caso03: se agrega estructura de grafo

--- Laboratorios/Lab 07/Caso03/algorithms.py
+++ Laboratorios/Lab 07/Caso03/algorithms.py
@@ -1,3 +1,58 @@
+from graph import GraphLink, Vertex
+import heapq
+
+# Algoritmo Dijkstra - Programación voraz
+def dijkstra(grafo: GraphLink, inicio):
+    initial_vertex: Vertex = grafo.search_vertex(inicio)
+    if not initial_vertex:
+        return {}, {}
+
+    dist = {v.data: float('inf') for v in grafo.vertices}
+    prev = {v.data: None for v in grafo.vertices}
+    dist[inicio] = 0
+
+    pq = [(0, initial_vertex)]
+
+    while pq:
+        actual_dist, actual_vertex = heapq.heappop(pq)
+
+        if actual_dist > dist[actual_vertex.data]:
+            continue
+
+        for edge in actual_vertex.adj_list:
+            vecino = edge.dest
+            peso = edge.weight
+            new_dist = actual_dist + peso
+
+            if new_dist < dist[vecino.data]:
+                dist[vecino.data] = new_dist
+                prev[vecino.data] = actual_vertex.data
+                heapq.heappush(pq, (new_dist, vecino))
+
+    return dist, prev
+
+
+# Algoritmo para transformar resultado de Dijkstra
+def grafo_dijkstra(grafo: GraphLink, dist, prev):
+    nuevo_grafo = GraphLink()
+
+    for v in grafo.vertices:
+        nuevo_grafo.insert_vertex(v.data)
+
+    for nodo, padre in prev.items():
+        if padre is not None:
+            peso = None
+            v_padre = grafo.search_vertex(padre)
+            for edge in v_padre.adj_list:
+                if edge.dest.data == nodo:
+                    peso = edge.weight
+                    break
+            if peso is not None:
+                nuevo_grafo.insert_edge(padre, nodo, peso)
+
+    return nuevo_grafo
+
+# Algoritmo de la mochila - Programación Dinámica
 def backpack(peso, valor, W):
     n = len(peso)
     
