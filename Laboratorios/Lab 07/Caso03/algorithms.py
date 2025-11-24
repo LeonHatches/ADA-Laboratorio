4b78e3dd73948d89ddf052831c383594e4dab132
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-11-24 07:33:54 -0500

    Lab 07 - Caso03: se organiza código

--- Laboratorios/Lab 07/Caso03/algorithms.py
+++ Laboratorios/Lab 07/Caso03/algorithms.py
@@ -78,3 +78,58 @@ def backpack(pedidos, W):
 
     objetos.reverse()
     return dp[n][W], objetos
+
+# Algoritmo para hacer un recorrido con el vecino más cercano
+def nearest_neighbor(grafo, start, zonas : list[str]):
+    recorrido = [start]
+    actual = start
+
+    while zonas:
+        dist, prev = dijkstra(grafo, actual)
+        min_zona = zonas[0]
+        min_dist = dist[min_zona]
+
+        for zona in zonas:
+            if dist[zona] < min_dist:
+                min_dist = dist[zona]
+                min_zona = zona
+
+        recorrido.append(min_zona)
+        actual = min_zona
+        zonas.remove(min_zona)
+
+    return recorrido
+
+def reconstruir_camino(prev, destino):
+    camino = []
+    actual = destino
+    while actual is not None:
+        camino.append(actual)
+        actual = prev[actual]
+    camino.reverse()
+    return camino
+
+def ruta_completa(grafo, recorrido):
+    ruta_final = []
+
+    for i in range(len(recorrido) - 1):
+        origen = recorrido[i]
+        destino = recorrido[i+1]
+
+        dist, prev = dijkstra(grafo, origen)
+        camino = reconstruir_camino(prev, destino)
+
+        # Evitar repetir el nodo inicial
+        if ruta_final and camino[0] == ruta_final[-1]:
+            ruta_final.extend(camino[1:])
+        else:
+            ruta_final.extend(camino)
+
+    return ruta_final
+
+def reconstruir_grafo_recorrido(grafo : GraphLink, ruta):
+    for i in range(len(ruta) - 1):
+        grafo.insert_edge(ruta[i], ruta[i+1], 0)
+
+    return grafo
+
